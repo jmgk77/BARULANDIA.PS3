68788395c1d9ca80805caa02e704190761baa366
JMGK <jmgk77@gmail.com>
2020-11-05 14:20:19 -0300

    saving in thread

--- source/save.cpp
+++ source/save.cpp
@@ -1,6 +1,6 @@
 #include "save.h"
 
-void save_png(SDL_Surface *screen) {
+int do_save_png(void *screen) {
   char buf[128];
   time_t rawtime;
   struct tm *t;
@@ -11,8 +11,18 @@ void save_png(SDL_Surface *screen) {
   snprintf(buf, 128, "%sSAVEDATA/DRAW_%d_%02d_%02d_%02d_%02d_%02d%s", DATA_PATH,
            t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min,
            t->tm_sec, GRAPH_EXT);
-  dbglogger_printf("SAVING: %s\n", buf);
+  dbglogger_printf("THREAD SAVING: %s\n", buf);
 
   // undocumented SDL_Image function...
-  IMG_SavePNG(screen, buf);
+  IMG_SavePNG((SDL_Surface *)screen, buf);
+  return 0;
+}
+
+void save_png(SDL_Surface *screen) {
+
+  SDL_Thread *thread = SDL_CreateThread(do_save_png, "SAVE_PNG", screen);
+
+  if (thread == NULL) {
+    dbglogger_printf("SDL_CreateThread failed: %s\n", SDL_GetError());
+  }
 }
